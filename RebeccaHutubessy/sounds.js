5abd688fd85c0d77822fd37947ab5be33e662afe
Rebecca Hutubessy <45396621+hutuber001@users.noreply.github.com>
2020-05-13 20:47:04 +0200

    Add files via upload

---
+++ RebeccaHutubessy/sounds.js
@@ -0,0 +1,31 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+
+
+sounds = [
+    0,
+    9,
+    10,
+    12,
+    13,
+    14,
+    15,
+    16.5,
+    18,
+    24,
+    25.8,
+    26,
+    27,
+    31,
+    32,
+    33,
+    34,
+    50,
+    76,
+    114,
+    115,
+    120,
+    122, 22
+
+
+];
